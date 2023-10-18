64e005f783746a31e07e60c5598e7d1317027234
vierageorge <joviera@gmail.com>
2023-10-18 14:02:37 -0500

    Adds Editor and Autosave

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,19 +1,32 @@
 import { getUserByClerkId } from '@/utils/auth';
+import Editor from '@/components/Editor';
 import { prisma } from '@/utils/db';
+import { JournalEntry } from '@prisma/client';
 
 type EntryPageParams = {
   id: string;
 };
 
-const getEntry = async (id: string) => {
+const getEntry = async (id: string): Promise<JournalEntry> => {
   const user = await getUserByClerkId();
-  const entry = prisma.journalEntry.findUnique({
-    where: { id, userId: user.id },
+  const entry = prisma.journalEntry.findUniqueOrThrow({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
   });
+  return entry;
 };
 
-const EntryPage = ({ params }: { params: EntryPageParams }) => {
-  return <div>{params.id}</div>;
+const EntryPage = async ({ params }: { params: EntryPageParams }) => {
+  const entry = await getEntry(params.id);
+  return (
+    <div className="w-full h-full">
+      <Editor entry={entry} />
+    </div>
+  );
 };
 
 export default EntryPage;
--- utils/api.ts
+++ utils/api.ts
@@ -1,3 +1,5 @@
+import { revalidatePath } from 'next/cache';
+
 const createUrl = (path: string): string => {
   return window.location.origin + path;
 };
@@ -5,6 +7,21 @@ const createUrl = (path: string): string => {
 export const createNewEntry = async () => {
   const res = await fetch(new Request(createUrl('/api/journal')), {
     method: 'POST',
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    return data.data;
+  }
+  revalidatePath('/journal');
+};
+
+export const updateEntry = async (id: string, content: string) => {
+  const res = await fetch(new Request(createUrl(`/api/journal/${id}`)), {
+    method: 'PATCH',
+    body: JSON.stringify({
+      content,
+    }),
   });
 
   if (res.ok) {
